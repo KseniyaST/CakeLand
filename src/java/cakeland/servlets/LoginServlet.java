6b9f7fa570237325ed822e150d0317a8ae000179
AlenaMr <37691138+AlenaMr@users.noreply.github.com>
2018-05-05 14:51:10 +0600

    Сервлет

---
+++ src/java/cakeland/servlets/LoginServlet.java
@@ -0,0 +1,55 @@
+package cakeland.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+
+@WebServlet(name = "Login", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean isAuth = WebUtils.checkUser(request);
+        if (isAuth) {
+            response.sendRedirect(request.getContextPath());
+        } else {
+            response.sendRedirect("login.jsp");
+        }
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        String session = WebUtils.auth(login, password);
+        if (session != null) {
+            response.addCookie(new Cookie("cakeses", session));
+            response.sendRedirect(request.getContextPath());
+        }else{
+            response.sendError(400);
+        }
+    }
+    
+}
